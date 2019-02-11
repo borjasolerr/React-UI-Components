9440a9833ffbdf25ddccace22599473ae6efdd7a
borjasolerr <borjasolrey@gmail.com>
2019-02-11 17:24:21 +0100

    Project II - React Calculator finished mvp

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,16 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+
+function ActionButton() {
+  return (
+    <div className="btn_action--container">
+      <button className="btn btn_action--key">&divide;</button>
+      <button className="btn btn_action--key">&times;</button>
+      <button className="btn btn_action--key">&minus;</button>
+      <button className="btn btn_action--key">&#43;</button>
+      <button className="btn btn_action--key">&#61;</button>
+    </div>
+  );
+}
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,8 @@
-import React from 'react';
-import './Display.css';
+import React from "react";
+import "./Display.css";
+
+function Display() {
+  return <button className="display">0</button>;
+}
+
+export default Display;
