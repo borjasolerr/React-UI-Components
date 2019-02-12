fb37dbd412a73eefcb89f5323fc77eb705edb576
borjasolerr <borjasolrey@gmail.com>
2019-02-12 16:06:08 +0100

    Stretch goals: added footer

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h1>Footer</h1>
+      <p>Come back and put icons here later</p>
+    </div>
+  );
+}
+
+export default Footer;
