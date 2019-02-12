38a3fb0f080e7db2448f0881b227978881a5ba2a
Samarvir Singh <samarvir1996@gmail.com>
2019-02-12 17:57:22 +0530

    Revert "Finished mvp"

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,12 +1,2 @@
-import React from "react";
-import "./Header.css";
-
-function ImageThumbnail() {
-  return (
-    <div>
-      <img src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" alt="Lambda School Logo" />
-    </div>
-  );
-}
-
-export default ImageThumbnail;
+import React from 'react';
+import './Header.css';
--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,16 +1,2 @@
-import React from "react";
-import "./Button.css";
-
-function ActionButton() {
-  return (
-    <div className="btn_action--container">
-      <button className="btn btn_action--key">&divide;</button>
-      <button className="btn btn_action--key">&times;</button>
-      <button className="btn btn_action--key">&minus;</button>
-      <button className="btn btn_action--key">&#43;</button>
-      <button className="btn btn_action--key">&#61;</button>
-    </div>
-  );
-}
-
-export default ActionButton;
+import React from 'react';
+import './Button.css';
